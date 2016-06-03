39e4767566310351cf3a890b79c58b11ef45bd3d
Nathaniel Knight <nknight@siaras.com>
2016-06-03 09:47:39 -0700

    Add example project (for building example wheels)

---
+++ greeter/greeter/__init__.py
@@ -0,0 +1,8 @@
+"""Functions for greeting ppl in various languages"""
+
+def english(name):
+    return "Hello, {}".format(name)
+
+
+def french(name):
+    return "Bonjour, {}".format(name)
---
+++ greeter/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(
+    name="greeter",
+    packages=["greeter"],
+    version="0.0.0",
+)
